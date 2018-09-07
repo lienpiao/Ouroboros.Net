b002aa9cca6dcbb8b2f15fa803e7414da0ceb99a
Goosander <511169817@qq.com>
2018-09-07 16:17:22 +0800

    Model权限设计

---
+++ src/Ouroboros.Model/EnumList/ActionType.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ouroboros.Model.EnumList
+{
+    /// <summary>
+    /// 权限类型
+    /// </summary>
+    public enum ActionType
+    {
+        /// <summary>
+        /// 菜单
+        /// </summary>
+        Menu = 10101,
+
+        /// <summary>
+        /// 功能
+        /// </summary>
+        Operation = 10102,
+
+        /// <summary>
+        /// 页面元素
+        /// </summary>
+        PageElement = 10103,
+
+        /// <summary>
+        /// 文件
+        /// </summary>
+        File = 10104
+    }
+}
---
+++ src/Ouroboros.Model/EnumList/HttpMethodType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ouroboros.Model.EnumList
+{
+    /// <summary>
+    /// 请求方法类型
+    /// </summary>
+    public enum HttpMethodType
+    {
+        /// <summary>
+        /// get
+        /// </summary>
+        HttpGet = 10201,
+
+        /// <summary>
+        /// post
+        /// </summary>
+        HttpPost = 10202
+    }
+}
