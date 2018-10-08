fc106a0e8d0c23d2ca7b9ba0fcc28373dc6ee475
Goosander <511169817@qq.com>
2018-10-08 09:35:05 +0800

    当前EF上下文的抽象工厂接口

---
+++ src/Ouroboros.IDAL/IDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace Ouroboros.IDAL
+{
+    /// <summary>
+    /// 当前EF上下文的抽象工厂接口
+    /// </summary>
+    public interface IDbContextFactory
+    {
+        /// <summary>
+        /// 获取当前上下文的唯一实例
+        /// </summary>
+        /// <returns></returns>
+        DbContext GetCurrentThreadInstance();
+    }
+}
