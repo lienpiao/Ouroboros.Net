ab148c65b2acf333d9935ab50521aec92a6538d8
Goosander <511169817@qq.com>
2018-10-09 12:05:13 +0800

    控制器基类

---
+++ src/Ouroboros.Mvc/Infrastucture/BaseController .cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Ouroboros.Mvc.Infrastucture
+{
+    /// <summary>
+    /// Controller基类
+    /// </summary>
+    public class BaseController : Controller
+    {
+        public BaseController()
+        {
+            this.DisposableObjects = new List<IDisposable>();
+        }
+
+        #region 手动垃圾回收的机制
+        protected IList<IDisposable> DisposableObjects { get; private set; }
+
+        protected void AddDisposableObject(object obj)
+        {
+            IDisposable disposable = obj as IDisposable;
+            if (disposable != null)
+            {
+                this.DisposableObjects.Add(disposable);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                foreach (IDisposable obj in this.DisposableObjects)
+                {
+                    if (null != obj)
+                    {
+                        obj.Dispose();
+                    }
+                }
+            }
+            base.Dispose(disposing);
+        }
+        #endregion
+    }
+}
