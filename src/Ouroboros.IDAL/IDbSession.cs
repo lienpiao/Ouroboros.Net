6679567af7f1d03cf36940128549e593ef05abbc
Goosander <511169817@qq.com>
2018-10-09 10:33:41 +0800

    创建DbSession层，数据访问层的统一入口
    DbSession层主要做了3件事：
    1、提交所有变化
    2、拿到各个IXXXDao类型
    3、执行SQL语句

---
+++ src/Ouroboros.IDAL/IDbSession.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.SqlClient;
+
+namespace Ouroboros.IDAL
+{
+    /// <summary>
+    /// IDbSession接口，数据库访问层的统一入口
+    /// </summary>
+    public interface IDbSession
+    {
+        #region 获取所有的IDao
+        ISysUserDao SysUserDao { get; set; }
+        ISysRoleDao SysRoleDao { get; set; }
+        ISysActionDao SysActionDao { get; set; }
+        ISysUserActionDao SysUserActionDao { get; set; }
+        #endregion
+
+        /// <summary>
+        /// 执行sql语句
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="paras"></param>
+        /// <returns></returns>
+        int ExeucteSql(string sql, params SqlParameter[] paras);
+
+        /// <summary>
+        /// 保存所有变化
+        /// </summary>
+        /// <returns></returns>
+        int SaveChanges();
+    }
+}
