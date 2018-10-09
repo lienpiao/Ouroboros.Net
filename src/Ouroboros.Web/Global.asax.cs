b2e996cd442beed814fc501df29ed5b7d86b915c
Goosander <511169817@qq.com>
2018-10-09 12:04:58 +0800

    自定义Ninject控制器工厂

--- src/Ouroboros.Web/Global.asax.cs
+++ src/Ouroboros.Web/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ouroboros.Mvc.Infrastucture;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,8 @@ namespace Ouroboros.Web
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //告诉MVC用新建的类(NinjectControllerFactory)来创建控制器对象。
+            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
         }
     }
 }
