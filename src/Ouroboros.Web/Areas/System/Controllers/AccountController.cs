22fa2003e9c9cdd07e8ac7afa6777c64e3562cf2
Goosander <511169817@qq.com>
2018-10-10 15:13:27 +0800

    登录界面

---
+++ src/Ouroboros.Web/Areas/System/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ouroboros.Web.Areas.System.Controllers
+{
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
